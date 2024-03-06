6a51435882867e88203e6851a882d28e5b0e9e86
chengetanai <mukanhairic@gmail.com>
2024-03-06 14:21:27 +0200

    add cors policy

--- main.py
+++ main.py
@@ -11,9 +11,18 @@ from app.routers import (
     leave,
     clocking,
 )
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/home")

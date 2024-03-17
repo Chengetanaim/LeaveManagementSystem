8ff037f5234e565817ac837a89f4b5bfae4d02eb
Tawanda M. Nyoni <tmnyoni@outlook.com>
2024-03-17 10:16:35 +0200

    add error404

--- app/routers/sell_leave.py
+++ app/routers/sell_leave.py
@@ -5,71 +5,89 @@ from .. import schemas, database, models, oauth2
 router = APIRouter(prefix="/sell-leave", tags=["Sell Leave"])
 
 
+class Error404(HTTPException):
+    def __init__(self, message: str):
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=message)
+
+
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def sell_leave(
     sell_leave_details: schemas.SellLeave,
     db: Session = Depends(database.get_db),
     current_user: schemas.UserOut = Depends(oauth2.get_current_user),
 ):
-    current_employee = (
+    employee_sending_leave = (
         db.query(models.Employee)
         .filter(models.Employee.user_id == current_user.id)
         .first()
     )
-    if not current_employee:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="This user does not have an employee profile.",
-        )
-    target_employee = (
+
+    if not employee_sending_leave:
+        raise Error404(message="User does not have employee details")
+
+    employee_receiving_leave = (
         db.query(models.Employee)
         .filter(models.Employee.id == sell_leave_details.employee_id)
         .first()
     )
-    if not target_employee:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Employee does not exist."
-        )
-    leave_type = (
+
+    if not employee_receiving_leave:
+        raise Error404(message="Receiving Employee does not exist")
+
+    leave_type_to_sell = (
         db.query(models.LeaveType)
         .filter(models.LeaveType.id == sell_leave_details.leave_type_id)
         .first()
     )
-    if not leave_type:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Leave does not exist"
+
+    if not leave_type_to_sell:
+        raise Error404(message="Leave type does not exist")
+
+    if leave_type_to_sell.leave_type == "Annual leave":
+        employee_sending_leave_grade = (
+            db.query(models.EmployeeGrade)
+            .filter(
+                models.EmployeeGrade.grade_id == employee_sending_leave.grade_id,
+                models.EmployeeGrade.employee_id == employee_sending_leave.id,
+            )
+            .first()
         )
-    if leave_type.leave_type == "Annual leave":
-        current_employee_grade_query = db.query(models.EmployeeGrade).filter(
-            models.EmployeeGrade.grade_id == current_employee.grade_id,
-            models.EmployeeGrade.employee_id == current_employee.id,
-        )
-        current_employee_grade = current_employee_grade_query.first()
-        target_employee_grade_query = db.query(models.EmployeeGrade).filter(
-            models.EmployeeGrade.grade_id == target_employee.grade_id,
-            models.EmployeeGrade.employee_id == target_employee.id,
-        )
-        target_employee_grade = target_employee_grade_query.first()
+
+        if employee_sending_leave_grade is None:
+            raise Error404(message="Employee does not have a grade")
+
         current_grade = (
             db.query(models.Grade)
-            .filter(models.Grade.id == current_employee.grade_id)
+            .filter(models.Grade.id == employee_sending_leave.grade_id)
             .first()
         )
+
+        employee_receiving_leave_grade = (
+            db.query(models.EmployeeGrade)
+            .filter(
+                models.EmployeeGrade.grade_id == employee_receiving_leave.grade_id,
+                models.EmployeeGrade.employee_id == employee_receiving_leave.id,
+            )
+            .first()
+        )
+
         target_grade = (
             db.query(models.Grade)
-            .filter(models.Grade.id == target_employee.grade_id)
+            .filter(models.Grade.id == employee_receiving_leave.grade_id)
             .first()
         )
-        if current_employee_grade:
+
+        if employee_sending_leave_grade:
             days_left = (
-                current_employee_grade.days_left - sell_leave_details.number_of_days
+                employee_sending_leave_grade.days_left
+                - sell_leave_details.number_of_days
             )
             if days_left < 0:
                 raise HTTPException(
                     status_code=status.HTTP_400_BAD_REQUEST,
                     detail=f"Operation cannot be peroformed, you will have remaining balance of {days_left} leave days left",
                 )
-            current_employee_grade_query.update(
+            employee_sending_leave_grade.update(
                 {"days_left": days_left}, synchronize_session=False
             )
             db.commit()
@@ -82,17 +100,17 @@ def sell_leave(
                     detail=f"Operation cannot be peroformed, you will have remaining balance of {days_left} leave days left",
                 )
             new_current_employee_grade = models.EmployeeGrade(
-                employee_id=current_employee.id,
+                employee_id=employee_sending_leave.id,
                 grade_id=current_grade.id,
                 days_left=days_left,
             )
             db.add(new_current_employee_grade)
             db.commit()
             db.refresh(new_current_employee_grade)
-        if target_employee_grade:
-            target_employee_grade_query.update(
+        if employee_receiving_leave_grade:
+            employee_receiving_leave_grade.update(
                 {
-                    "days_left": target_employee_grade.days_left
+                    "days_left": employee_receiving_leave_grade.days_left
                     + sell_leave_details.number_of_days
                 },
                 synchronize_session=False,
@@ -100,7 +118,7 @@ def sell_leave(
             db.commit()
         else:
             new_target_employee_grade = models.EmployeeGrade(
-                employee_id=target_employee.id,
+                employee_id=employee_receiving_leave.id,
                 grade_id=target_grade.id,
                 days_left=target_grade.leave_days + sell_leave_details.number_of_days,
             )
@@ -108,15 +126,17 @@ def sell_leave(
             db.commit()
             db.refresh(new_target_employee_grade)
         return {"message": "Annual Leave has been sold."}
+
     current_employee_leave_query = db.query(models.LeaveDaysLeft).filter(
-        models.LeaveDaysLeft.employee_id == current_employee.id,
-        models.LeaveDaysLeft.leave_type_id == leave_type.id,
+        models.LeaveDaysLeft.employee_id == employee_sending_leave.id,
+        models.LeaveDaysLeft.leave_type_id == leave_type_to_sell.id,
     )
+
     current_employee_leave = current_employee_leave_query.first()
 
     target_employee_leave_query = db.query(models.LeaveDaysLeft).filter(
-        models.LeaveDaysLeft.employee_id == target_employee.id,
-        models.LeaveDaysLeft.leave_type_id == leave_type.id,
+        models.LeaveDaysLeft.employee_id == employee_receiving_leave.id,
+        models.LeaveDaysLeft.leave_type_id == leave_type_to_sell.id,
     )
     target_employee_leave = target_employee_leave_query.first()
 
@@ -132,15 +152,15 @@ def sell_leave(
         )
         db.commit()
     else:
-        days_left = leave_type.leave_days - sell_leave_details.number_of_days
+        days_left = leave_type_to_sell.leave_days - sell_leave_details.number_of_days
         if days_left < 0:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail=f"Operation cannot be peroformed, you will have remaining balance of {days_left} leave days left",
             )
         new_current_employee_leave = models.LeaveDaysLeft(
-            employee_id=current_employee.id,
-            leave_type_id=leave_type.id,
+            employee_id=employee_sending_leave.id,
+            leave_type_id=leave_type_to_sell.id,
             days_left=days_left,
         )
         db.add(new_current_employee_leave)
@@ -158,11 +178,11 @@ def sell_leave(
         db.commit()
     else:
         new_target_employee_leave = models.LeaveDaysLeft(
-            employee_id=target_employee.id,
-            leave_type_id=leave_type.id,
-            days_left=leave_type.leave_days + sell_leave_details.number_of_days,
+            employee_id=employee_receiving_leave.id,
+            leave_type_id=leave_type_to_sell.id,
+            days_left=leave_type_to_sell.leave_days + sell_leave_details.number_of_days,
         )
         db.add(new_target_employee_leave)
         db.commit()
         db.refresh(new_target_employee_leave)
-    return {"message": "Leave days successfully sold!"}
+    return new_current_employee_leave

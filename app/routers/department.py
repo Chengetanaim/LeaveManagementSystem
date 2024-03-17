91344f060ba4c278e7e7a0359d9432e3e6052ea2
Tawanda M. Nyoni <tmnyoni@outlook.com>
2024-03-17 10:16:51 +0200

    Refactor grade router

--- app/routers/department.py
+++ app/routers/department.py
@@ -4,11 +4,15 @@ from .. import database, models, oauth2, schemas
 from typing import List
 from fastapi import Response
 
-router = APIRouter(prefix='/departments', tags=['Departments'])
+router = APIRouter(prefix="/departments", tags=["Departments"])
 
 
-@router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.DepartmentOut)
-def create_department(department_details:schemas.Department, db:Session=Depends(database.get_db), current_user:int=Depends(oauth2.get_current_user)):
+@router.post(
+    "/", status_code=status.HTTP_201_CREATED, response_model=schemas.DepartmentOut
+)
+def create_department(
+    department_details: schemas.Department, db: Session = Depends(database.get_db)
+):
     new_department = models.Department(**department_details.dict())
     db.add(new_department)
     db.commit()
@@ -16,36 +20,69 @@ def create_department(department_details:schemas.Department, db:Session=Depends(
     return new_department
 
 
-@router.get('/', response_model=List[schemas.DepartmentOut])
-def get_departments(db:Session=Depends(database.get_db), current_user:int=Depends(oauth2.get_current_user)):
+@router.get("/", response_model=List[schemas.DepartmentOut])
+def get_departments(
+    db: Session = Depends(database.get_db),
+):
     departments = db.query(models.Department).all()
     return departments
 
 
-@router.get('/{department_id}', response_model=schemas.DepartmentOut)
-def get_department(department_id: int,db:Session=Depends(database.get_db), current_user:int=Depends(oauth2.get_current_user)):
-    department = db.query(models.Department).filter(models.Department.id == department_id).first()
+@router.get("/{department_id}", response_model=schemas.DepartmentOut)
+def get_department(
+    department_id: int,
+    db: Session = Depends(database.get_db),
+    current_user: int = Depends(oauth2.get_current_user),
+):
+    department = (
+        db.query(models.Department)
+        .filter(models.Department.id == department_id)
+        .first()
+    )
     if not department:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Department with id: {department_id} does not exist")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Department with id: {department_id} does not exist",
+        )
     return department
 
-@router.put('/{department_id}')
-def update_department(department_id:int, department_details:schemas.Department,db:Session=Depends(database.get_db), current_user:int=Depends(oauth2.get_current_user)):
-    department_query = db.query(models.Department).filter(models.Department.id == department_id)
+
+@router.put("/{department_id}")
+def update_department(
+    department_id: int,
+    department_details: schemas.Department,
+    db: Session = Depends(database.get_db),
+    current_user: int = Depends(oauth2.get_current_user),
+):
+    department_query = db.query(models.Department).filter(
+        models.Department.id == department_id
+    )
     department = department_query.first()
     if not department:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Department with id: {department_id} does not exist")
-    department_query.update(department_details.dict(), synchronize_session=False)
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Department with id: {department_id} does not exist",
+        )
+    department_query.update(department_details.model_dump(), synchronize_session=False)
     db.commit()
     return {"message": "Department updated successfully!"}
 
 
-@router.delete('/{department_id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_department(department_id:int,db:Session=Depends(database.get_db), current_user:int=Depends(oauth2.get_current_user)):
-    department_query = db.query(models.Department).filter(models.Department.id == department_id)
+@router.delete("/{department_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_department(
+    department_id: int,
+    db: Session = Depends(database.get_db),
+    current_user: int = Depends(oauth2.get_current_user),
+):
+    department_query = db.query(models.Department).filter(
+        models.Department.id == department_id
+    )
     department = department_query.first()
     if not department:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Department with id: {department_id} does not exist")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Department with id: {department_id} does not exist",
+        )
     department_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
--- app/routers/grade.py
+++ app/routers/grade.py
@@ -4,45 +4,73 @@ from .. import database, models, oauth2, schemas
 from typing import List
 from fastapi.responses import Response
 
-router = APIRouter(prefix='/grades', tags=['Grades'])
+router = APIRouter(prefix="/grades", tags=["Grades"])
 
-@router.post('/', response_model=schemas.GradeOut, status_code=status.HTTP_201_CREATED)
-def create_grade(grade_details:schemas.Grade, db:Session=Depends(database.get_db), current_user:int=Depends(oauth2.get_current_user)):
+
+@router.post("/", response_model=schemas.GradeOut, status_code=status.HTTP_201_CREATED)
+def create_grade(grade_details: schemas.Grade, db: Session = Depends(database.get_db)):
     new_grade = models.Grade(**grade_details.dict())
     db.add(new_grade)
     db.commit()
     db.refresh(new_grade)
     return new_grade
 
-@router.get('/', response_model=List[schemas.GradeOut])
-def get_grades(db:Session=Depends(database.get_db), current_user:int=Depends(oauth2.get_current_user)):
+
+@router.get("/", response_model=List[schemas.GradeOut])
+def get_grades(
+    db: Session = Depends(database.get_db),
+):
     grades = db.query(models.Grade).all()
     return grades
 
-@router.get('/{grade_id}', response_model=schemas.GradeOut)
-def get_grade(grade_id:int, db:Session=Depends(database.get_db), current_user:int=Depends(oauth2.get_current_user)):
+
+@router.get("/{grade_id}", response_model=schemas.GradeOut)
+def get_grade(
+    grade_id: int,
+    db: Session = Depends(database.get_db),
+    current_user: int = Depends(oauth2.get_current_user),
+):
     grade = db.query(models.Grade).filter(models.Grade.id == grade_id).first()
     if not grade:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Grade with id: {grade_id} does not exist.")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Grade with id: {grade_id} does not exist.",
+        )
     return grade
 
-@router.put('/{grade_id}')
-def update_grade(grade_id:int, grade_details:schemas.Grade, db:Session=Depends(database.get_db), current_user:int=Depends(oauth2.get_current_user)):
+
+@router.put("/{grade_id}")
+def update_grade(
+    grade_id: int,
+    grade_details: schemas.Grade,
+    db: Session = Depends(database.get_db),
+    current_user: int = Depends(oauth2.get_current_user),
+):
     grade_query = db.query(models.Grade).filter(models.Grade.id == grade_id)
     grade = grade_query.first()
     if not grade:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Grade with id: {grade_id} does not exist.")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Grade with id: {grade_id} does not exist.",
+        )
     grade_query.update(grade_details.dict(), synchronize_session=False)
     db.commit()
     return {"message": "Grade has been successfully updated"}
 
-@router.delete('/{grade_id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_grade(grade_id:int, db:Session=Depends(database.get_db), current_user:int=Depends(oauth2.get_current_user)):
+
+@router.delete("/{grade_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_grade(
+    grade_id: int,
+    db: Session = Depends(database.get_db),
+    current_user: int = Depends(oauth2.get_current_user),
+):
     grade_query = db.query(models.Grade).filter(models.Grade.id == grade_id)
     grade = grade_query.first()
     if not grade:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Grade with id: {grade_id} does not exist.")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Grade with id: {grade_id} does not exist.",
+        )
     grade_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
-    
